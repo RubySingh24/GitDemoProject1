121aea866c7ee698f6e3874dc9971c420a9378ef
Rajeshwari <singh.rajeshwari2401@gmail.com>
2024-10-05 23:18:06 +0530

    add new check by developer1

--- src/main/java/com/git/GitDemoProject1/TestByDeveloper.java
+++ src/main/java/com/git/GitDemoProject1/TestByDeveloper.java
@@ -3,5 +3,6 @@ package com.git.GitDemoProject1;
 public class TestByDeveloper {
 public static void main(String[] args) {
 	System.out.println("This line is written by developer 1");
+	System.out.println("Add new check");
 }
 }
