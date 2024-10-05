112d2075a49c36756d69b56e2eb57a1163715793
Rajeshwari <singh.rajeshwari2401@gmail.com>
2024-10-05 22:06:15 +0530

    first commit

---
+++ src/main/java/com/git/GitDemoProject1/App.java
@@ -0,0 +1,14 @@
+package com.git.GitDemoProject1;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		String hungry="yes";
+		if(hungry=="yes") {
+			System.out.println("Eat something.....");
+		}else {
+			System.out.println("DO work ......");
+		}
+		System.out.println("Ending of main method");
+	}
+}
